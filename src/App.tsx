d1249588be76123175e7a2a4109459d7e3243d53
dima-sheiko <sheydim@gmail.com>
2022-10-24 13:46:16 +0300

    Add basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,26 @@ import { Button } from './components/Button/Button';
 import { Header } from './components/Header/Header';
 import { Input } from './components/Input/Input';
 import { Results } from './components/Results/Results';
-import'./styles/styles.css';
+import './styles/styles.css';
 
 export const App = () => {
   return (
-    <main>
-      <Header />
-      <Input />
-      <Input />
-      <Input />
-      <Results />
-      <Results />
-      <Button />
-    </main>
+    <div className='wrapper'>
+      <main className='container'>
+        <div className='header-box'>
+          <Header />
+        </div>
+        <div className='inputs-box'>
+          <Input />
+          <Input />
+          <Input />
+        </div>
+        <div className='results-box'>
+          <Results />
+          <Results />
+          <Button />
+        </div>
+      </main>
+    </div>
   );
 };
