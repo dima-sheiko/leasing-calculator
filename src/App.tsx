68780e12e7a51c56bb93fbd1902220370818183f
dima-sheiko <sheydim@gmail.com>
2022-10-26 13:21:53 +0300

    Implement logic for inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Button } from './components/Button/Button';
 import { Header } from './components/Header/Header';
 import { Input } from './components/Input/Input';
 import { Results } from './components/Results/Results';
 import './styles/styles.css';
 
+// Logic
 export const App = () => {
+  const [carPrice, setCarPrice] = useState(1000000);
+  const [downPayment, setDownPayment] = useState(100000);
+  const [leaseTerm, setleaseTerm] = useState(60);
+
+  // Handlers
+  const handleCarPrice = (e: number) => {
+    if (downPayment < Math.round(e * 0.1)) {
+      setDownPayment(carPrice * 0.1);
+    }
+    if (downPayment > Math.round(e * 0.6)) {
+      setDownPayment(carPrice * 0.6);
+    }
+    setCarPrice(e);
+  };
+
+  const handleDownPayment = (e: number) => {
+    setDownPayment(e);
+  };
+
+  const handleLeaseTerm = (e: number) => {
+    setleaseTerm(e);
+  };
+
+  // Helpers
+  const calculateDPMin = () => {
+    const result = Math.round(carPrice * 0.1);
+    return result;
+  };
+
+  const calculateDPMax = () => {
+    const result = Math.round(carPrice * 0.6);
+    return result;
+  };
+
+  const calculateDPBadge = () => {
+    let result = Math.round((downPayment / carPrice) * 100);
+    if (result > 60) result = 60;
+    if (result < 10) result = 10;
+    return result;
+  };
+
   return (
     <div className='wrapper'>
       <main className='container'>
@@ -13,9 +55,33 @@ export const App = () => {
           <Header>Calculate the cost of a car lease</Header>
         </div>
         <div className='inputs-box'>
-          <Input />
-          <Input />
-          <Input />
+          <Input
+            value={carPrice}
+            onChange={handleCarPrice}
+            label='Car cost'
+            min={1000000}
+            max={6000000}
+            step={5000}
+            badge='$'
+          />
+          <Input
+            value={downPayment}
+            onChange={handleDownPayment}
+            label='Down payment'
+            min={calculateDPMin()}
+            max={calculateDPMax()}
+            step={500}
+            badge={`${calculateDPBadge()}%`}
+          />
+          <Input
+            value={leaseTerm}
+            onChange={handleLeaseTerm}
+            label='Lease term'
+            min={1}
+            max={60}
+            step={1}
+            badge={leaseTerm === 1 ? 'mo.' : 'mos.'}
+          />
         </div>
         <div className='results-box'>
           <Results />
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import { ChangeEvent } from 'react';
 
-export const Input = () => {
+// Types
+type InputProps = {
+  value: number;
+  onChange: (e: number) => void;
+  label: string;
+  min: number;
+  max: number;
+  step: number;
+  badge: string | number;
+};
+
+// Logic
+export const Input = ({
+  value,
+  onChange,
+  label,
+  min,
+  max,
+  step,
+  badge,
+}: InputProps) => {
+  // Handlers
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(Number(e.target.value));
+  };
+
+  const handleBlur = () => {
+    if (value < min) {
+      onChange(min);
+    }
+
+    if (value > max) {
+      onChange(max);
+    }
+  };
+
   return (
     <div>
-      <h3>Blab</h3>
-      <input type='text' />
-      <input type="range" />
+      <span>{label}</span>
+      <input
+        value={value}
+        onChange={handleInputChange}
+        onBlur={handleBlur}
+        type='text'
+        name='price input'
+      />
+      <input
+        value={value}
+        onChange={handleInputChange}
+        min={min}
+        max={max}
+        step={step}
+        type='range'
+        name='price slider'
+      />
+      {badge}
     </div>
   );
 };
